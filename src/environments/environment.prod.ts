859b0b180acf04ab61c77a6606b99696434d4ff3
TwumJnr <39347786+TwumJnr@users.noreply.github.com>
2021-10-25 13:14:44 +0000

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,7 @@ export const environment = {
   vnPath: '/assets/media',
   env: '10',
   api: `https://click-order-eat.bodacommunity.io:10484/api-z/`,
-  _api: `https://click-order-eat.bodacommunity.io:10484`,
+  // _api: `https://click-order-eat.bodacommunity.io:10484`,
+  _api: `https://delivery-db.herokuapp.com`,
   api_1: `https://smartcityapi.bodacommunity.io:8080/api-1/`,
 };
